260ec37b35d17ee6e400635e8e4f67cfed5c7392
DONrichD <123484028+DONrichD@users.noreply.github.com>
2023-06-02 17:16:53 -0400

    Delete float volt = 120;.py

--- float volt = 120;.py
+++
@@ -1,7 +0,0 @@
-  float volt = 120;
-  float amp = 20;
-  int pf = 100;
-  float theta = acos(pf / 100);
-  float vars = volt * amp * sin(theta);
-  float Cap = vars / (2 * 3.14 * volt * volt);
-    print(Cap);
